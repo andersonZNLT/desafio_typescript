9e7427cfbe5637b39af079515c1764e4c6df4b3f
andersonZNLT <anderson.znlt@gmail.com>
2022-06-27 21:01:36 -0300

    resolução desafio_2

---
+++ desafios/desafio_2.ts
@@ -0,0 +1,55 @@
+/*
+// Como podemos melhorar o esse código usando TS? 
+
+let pessoa1 = {};
+pessoa1.nome = "maria";
+pessoa1.idade = 29;
+pessoa1.profissao = "atriz"
+
+let pessoa2 = {}
+pessoa2.nome = "roberto";
+pessoa2.idade = 19;
+pessoa2.profissao = "Padeiro";
+
+let pessoa3 = {
+    nome: "laura",
+    idade: "32",
+    profissao: "Atriz"
+};
+
+let pessoa4 = {
+    nome = "carlos",
+    idade = 19,
+    profissao = "padeiro"
+}
+*/
+
+type Humano = {
+    nome: string,
+    idade: number,
+    profissao: string
+}
+
+let pessoa1: Humano = {
+    nome: 'maria',
+    idade: 29,
+    profissao: 'atriz'
+}
+
+let pessoa2: Humano = {
+    nome: 'roberto',
+    idade: 19,
+    profissao: 'padeiro'
+}
+
+let pessoa3: Humano = {
+    nome: 'laura',
+    idade: 32,
+    profissao: 'atriz'
+}
+
+let pessoa4: Humano = {
+    nome: "carlos",
+    idade: 19,
+    profissao: 'padeiro'
+}
